8da0d60ceb302b7e9d4424dbf05164446be286b1
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2025-02-19 18:18:21 -0600

    Add files via upload

---
+++ DragAndDrop.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+// Código para usarse en juegos drag and drop.
+
+class DraggableObject {
+public:
+    virtual void onDropEffect() = 0;
+    virtual ~DraggableObject() = default;
+};
+
+class Warehouse{
+public:
+    virtual void getItems() = 0;
+    virtual void produce() = 0;
+    virtual ~Warehouse() = default;
+};
+
+class Seeds : public DraggableObject {
+public:
+    void onDropEffect() override {
+        cout << "Planted a pot" << endl;
+    }
+};
+
+class Paint : public DraggableObject {
+    public:
+        void onDropEffect() override {
+            cout << "Painted a piece" << endl;
+        }
+    };
+
+class GardenWarehouse : public Warehouse {
+    public:
+        void getItems() override {
+            cout << "Seeds and fertilizer" << endl;
+        }
+
+        void produce() override {
+            cout << "A plant has been produced" << endl;
+        }
+};
+
+class ConstructionWarehouse : public Warehouse {
+    public:
+        void getItems() override {
+            cout << "Paint, hammer" << endl;
+        }
+
+        void produce() override {
+            cout << "A toy has been produced" << endl;
+        }
+};
